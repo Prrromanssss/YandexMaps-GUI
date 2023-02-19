228bcd5477b0b8f2585437e52999ed56385ac9ae
Sourr_cream <roman_boyko.48@mail.ru>
2023-02-19 22:03:41 +0300

    add button to  change view

--- main.py
+++ main.py
@@ -2,74 +2,101 @@ import os
 import sys
 
 import pygame
+import pygame_gui as p_gui
 import requests
 
+from settings import HEIGHT, VIEWS, WIDTH
 
-def get_map(coord_x, coord_y, x, y):
+
+def get_map(coord_x, coord_y, size_x, size_y, view):
     map_request = (
         f"https://static-maps.yandex.ru/1.x/"
-        f"?ll={coord_x},{coord_y}&spn={x},{y}&l=sat"
+        f"?ll={coord_x},{coord_y}&spn={size_x},{size_y}&l={view}"
         )
     return map_request
 
 
-def show_map(screen, coord_x, coord_y,x, y):
+def show_map(screen, coord_x, coord_y, size_x, size_y, view):
     map_file = "map.png"
-    response = requests.get(get_map(coord_x, coord_y,x, y))
+    response = requests.get(get_map(coord_x, coord_y, size_x, size_y, view))
     if not response:
         print("Ошибка выполнения запроса:")
-        print(get_map(133.795384, -25.694768, 20, 20))
+        print(get_map(coord_x, coord_y, size_x, size_y))
         print("Http статус:", response.status_code, "(", response.reason, ")")
         sys.exit(1)
 
     with open(map_file, "wb") as file:
         file.write(response.content)
 
-    screen.blit(pygame.image.load(map_file), (0, 0))
+    screen.blit(pygame.image.load(map_file), (80, 0))
 
 
 def main():
-    screen = pygame.display.set_mode((600, 450))
-    x, y = 20, 20
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
+    manager = p_gui.UIManager((WIDTH, HEIGHT))
+    change_view_button = p_gui.elements.UIButton(
+        relative_rect=pygame.Rect(5, 5, 70, 30),
+        text='View',
+        manager=manager,
+    )
+    size_x, size_y = 20, 20
     coord_x, coord_y = 133.795384, -25.694768
-    show_map(screen, coord_x, coord_y, x, y)
     duration = 1
+    view = VIEWS[0]
+    show_map(screen, coord_x, coord_y, size_x, size_y, view)
+
     while True:
+        time_delta = clock.tick(60) / 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 os.remove("map.png")
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_PAGEUP:
-                    x /= 1.5
-                    y /= 1.5
+                    size_x /= 1.5
+                    size_y /= 1.5
                 if event.key == pygame.K_PAGEDOWN:
-                    x *= 1.5
-                    if x > 90:
-                        x = 90
-                    y *= 1.5
-                    if y > 90:
-                        y = 90
+                    size_x *= 1.5
+                    if size_x > 90:
+                        size_x = 90
+                    size_y *= 1.5
+                    if size_y > 90:
+                        size_y = 90
                 if event.key == pygame.K_RIGHT:
-                    coord_x += x * 2.
+                    coord_x += size_x * 2.
                     coord_x = -(-coord_x % 180) if coord_x >= 180 else coord_x
                 if event.key == pygame.K_LEFT:
-                    coord_x -= x * 2.
+                    coord_x -= size_x * 2.
                     coord_x = coord_x % 180 if coord_x <= -180 else coord_x
                 if event.key == pygame.K_UP:
-                    coord_y += y * 2. * duration
+                    coord_y += size_y * 2. * duration
                     if abs(coord_y) >= 90:
-                        coord_y = -(-coord_y % 90) if coord_y < 0 else -coord_y % 90
+                        coord_y = (
+                            -(-coord_y % 90) if coord_y < 0
+                            else -coord_y % 90
+                            )
                         coord_x = -coord_x
                         duration = -duration
                 if event.key == pygame.K_DOWN:
-                    coord_y -= y * 2. * duration
+                    coord_y -= size_y * 2. * duration
                     if abs(coord_y) >= 90:
-                        coord_y = -(-coord_y % 90) if coord_y < 0 else -coord_y % 90
+                        coord_y = (
+                            -(-coord_y % 90) if coord_y < 0
+                            else -coord_y % 90
+                            )
                         coord_x = -coord_x
                         duration = -duration
-                show_map(screen, coord_x, coord_y, x, y)
+                show_map(screen, coord_x, coord_y, size_x, size_y, view)
+            if event.type == pygame.USEREVENT:
+                if event.user_type == p_gui.UI_BUTTON_PRESSED:
+                    if event.ui_element == change_view_button:
+                        view = VIEWS[(VIEWS.index(view) + 1) % 3]
+                show_map(screen, coord_x, coord_y, size_x, size_y, view)
+            manager.process_events(event)
+        manager.update(time_delta)
+        manager.draw_ui(screen)
         pygame.display.flip()
 
 
---
+++ settings.py
@@ -0,0 +1,3 @@
+SIZE = WIDTH, HEIGHT = 600, 450
+
+VIEWS = ('map', 'sat', 'sat,skl')
